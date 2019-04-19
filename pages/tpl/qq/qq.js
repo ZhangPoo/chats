0971faec49be77115f442a31ac597efedb27b6e5
lopyu <38445951+ZhPo@users.noreply.github.com>
2019-04-19 10:55:57 +0800

    Update qq.js

--- pages/tpl/qq/qq.js
+++ pages/tpl/qq/qq.js
@@ -5,10 +5,38 @@ Page({
    * 页面的初始数据
    */
   data: {
-    singer:{
-      content:'呀拉索，那就是青藏高原！',
-      friend:'韩红和韩寒哈哈哈'
-    }
+    person:[
+      {
+      content:'今晚上一定要来吃我的十二道锋味',
+      friend:'谢霆锋',
+      time:'2019.4.19'
+    },
+    {
+      content:'我要去菜市场买菜！',
+      friend:'刘青云',
+      time:'2019.4.19'
+      },
+      {
+        content: '我就是鸽中之王！',
+        friend: '周杰伦',
+        time: '2019.4.19'
+      },
+      {
+        content: '给我一杯忘情水',
+        friend: '刘德华',
+        time: '2019.4.19'
+      },
+      {
+        content: '我对你爱爱爱不完！',
+        friend: '郭富城',
+        time: '2019.4.19'
+      },
+      {
+        content: '全日爱！！',
+        friend: '黎明',
+        time: '2019.4.19'
+      }
+    ]
   },
 
 })
