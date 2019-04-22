47076658371d845f226d93cd99a6a50e60fcfa0f
POPO <38445951+ZhangPoo@users.noreply.github.com>
2019-04-22 14:56:24 +0800

    Add files via upload

---
+++ pages/css/css.js
@@ -0,0 +1,79 @@
+// pages/css/css.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    person:[
+      {
+        title:"《JAY》",
+        author:"撒贝宁",
+        time:"2019.1.1",
+        image:'https://ss1.baidu.com/6ONXsjip0QIZ8tyhnq/it/u=3690978628,3697014167&fm=85&app=52&f=JPEG?w=121&h=75&s=7A783DC456433AF7C43134B70300D002'
+      },
+      {
+        title: "今晚别墅靠海！！！！",
+        author: "撒贝宁",
+        time: "2019.1.1",
+        image: 'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=777055527,1369955246&fm=27&gp=0.jpg'
+      }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
